4f1af7b4c158e0cb57958de45dd51b0f1ee4dfef
Lucca Fagundes <luccapfagundes@gmail.com>
2025-04-11 22:41:09 -0300

    Add files via upload

---
+++ Solo Levelling/main.ts
@@ -0,0 +1,27 @@
+import { AtaqueFisico, MagiaAtaque, habilidadeProtagonista } from "./habilidades";
+import { MagiaCura } from "./habilidades";
+import { MagiaFortalecimento } from "./habilidades";
+import { Cacador } from "./cacador";
+import { Monstro } from "./monstro";
+
+const cacador1 = new Cacador('Raye', 4, 150, 15, 15, 25)
+const cacador2 = new Cacador('Roze', 4, 150, 20, 15, 20)
+const oProtagonista = new Cacador ('Sung Jin woo', 300, 5000, 2000, 2400, 360)
+
+const monstro1 = new Monstro('Colossus', 8, 260, 20, 26, 28)
+const monstro2 = new Monstro('Fluffal Wolf', 6, 150, 15, 20, 22)
+const monstro3 = new Monstro('Umastryx', 7, 270, 25, 20, 18)
+const estatuaDeDeus = new Monstro ('Estatua de Deus', 100, 1000, 500, 300, 300)
+
+const habilidade1 = new AtaqueFisico ('Cross Scissors','Ataque Cortante em forma de tesoura utilizando lâminas de metal', 2, 20)
+const habilidade2 = new AtaqueFisico ('Widow Anchor','Ataque em forma de garras que causa grande dano ao alvo', 3, 35)
+const habilidade3 = new MagiaAtaque ('Afterburnes','Ataque de explosão que destrói tudo nas proximidades', 3, 50)
+const habilidade4 = new MagiaAtaque ('Jamming Waves','Ataque em forma de onde de raios', 2, 18)
+const habilidade5 = new MagiaCura ('Ace Kaina','Armadura Mágica que cura o usuário', 4, 10)
+const habilidade6 = new MagiaFortalecimento ('Ace Kagari','Armadura mágica que fortalece o poder de ataque do usuário', 4, 10, 'forca')
+const habilidade7 = new MagiaFortalecimento ('Ace Shizuko','Armadura mágica que fortalece a resistência do usuário', 4, 10, 'resistencia')
+const habilidade8 = new MagiaFortalecimento ('Ace Hayate','Armadura mágica que aumenta a velocidade do usuário', 4, 15, 'velocidade')
+
+const monstroTeste = new Monstro('Hunter', 1, 0, /* zero de vida para funcionar a invocação */ 0, 10, 10)
+const arise = new habilidadeProtagonista('Arise', 'Invoca a sombra', 10)
+monstroTeste.serInvocado()
---
+++ Solo Levelling/monstro.ts
@@ -0,0 +1,64 @@
+import { Habilidade } from "./interfaceHabilidades"
+
+export class Monstro {
+    private nome: string
+    private nivel: number
+    private vida: number
+    private resistencia: number
+    private forca: number
+    private velocidade: number
+
+    constructor(nome: string, nivel: number, vida: number, resistencia: number, forca: number, velocidade: number) {
+        this.nome = nome
+        this.nivel = nivel
+        this.vida = vida
+        this.resistencia = resistencia
+        this.forca = forca
+        this.velocidade = velocidade
+    }
+
+    setNome(nomeMonstro: string) {
+        this.nome = nomeMonstro
+    }
+    setVida(vidaMonstro: number) {
+        this.vida = vidaMonstro
+    }
+    setForca(forcaMonstro: number) {
+        this.forca = forcaMonstro
+    }
+
+    getNome(): string {
+        return this.nome
+    }
+    getVida(): number {
+        return this.vida
+    }
+    getForca(): number {
+        return this.forca
+    }
+
+    receberDano(dano: number): void {
+        dano -= this.resistencia
+        this.vida -= dano
+        console.log(`${this.nome} está com ${this.vida} de vida`)
+    }
+
+    serInvocado(): void {
+        if (this.vida > 1) {
+            console.log('Esta habilidade pode apenas ser usadas em corpos')
+        } else {
+            let chanceDeInvocar = 0;
+            let index = 0;
+            while (index < 3 && chanceDeInvocar === 0) {
+                chanceDeInvocar = Math.floor(Math.random() * 2)
+                index++
+            } if (chanceDeInvocar === 1) {
+                this.nome = 'Shadow ' + this.nome;
+                this.vida = this.vida * 1.5;
+                this.forca = this.forca * 1.5;
+                console.log(`O monstro ${this.nome} foi invocado com sucesso`)
+            } chanceDeInvocar = 0
+            index = 0
+        }
+    }
+}
